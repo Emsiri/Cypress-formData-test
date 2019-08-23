5ac93baca6e7a4195c728e0759de4d996f23bde9
bradf <bradf@benon.com>
2019-08-23 15:11:13 +1000

    Added the tests and the form_request functions

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # FormData issue
 When submitting a multipart/formdata request in Cypress runner (aka cypress open) everything works fine.
 When you run it in the Cypress headless command (aka cypress run) the tests pass regardless of the assertion.
+## API details
+API is a public test endpoint that can be found here: https://petstore.swagger.io/
+With the Upload section here: https://petstore.swagger.io/#/pet/uploadFile
+## To test
+Run 
+`npx cypress open`
+The test should fail on the assertion "expected false to equal true" (in chrome)
+Now run the following
+`npx cypress run`
+The test will pass
+You can change the response code assertion to anything other than 200 and it will still pass
+## Notes
+It seems when running the test with electron in open mode also incorrectly passes the test.
+Also when clicking into the formData tests, Cypress has the message "No commands were issued in this test."
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,32 @@
 //
 // -- This is will overwrite an existing command --
 // Cypress.Commands.overwrite("visit", (originalFn, url, options) => { ... })
+
+// Performs an XMLHttpRequest instead of a cy.request (able to send data as FormData - multipart/form-data)
+Cypress.Commands.add("form_request", (formData, url) => {
+    return cy
+        .server()
+        .route("POST", url)
+        .as("formRequest")
+        .window()
+        .then(win => {
+            const xhr = new win.XMLHttpRequest();
+            xhr.open('POST', url);
+            xhr.send(formData);
+        })
+        .wait("@formRequest");
+});
+
+Cypress.Commands.add('form_request_alternative', (formData, url, done) => {
+    const xhr = new XMLHttpRequest();
+    const method = 'POST';
+    cy.task('log', url);
+    xhr.open(method, url, true);
+    xhr.onload = function () {
+        done(xhr);
+    };
+    xhr.onerror = function () {
+        done(xhr);
+    };
+    xhr.send(formData);
+})
